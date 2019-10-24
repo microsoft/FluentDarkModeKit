e6faa4ae1e420c6d031844e66d2a0bc9ad1cc7a7
Bei Li <kylinroc@gmail.com>
2019-10-24 22:45:10 +0800

    Create project

---
+++ DarkModeKit/DarkModeKit.h
@@ -0,0 +1,19 @@
+//
+//  DarkModeKit.h
+//  DarkModeKit
+//
+//  Created by Bei Li on 2019/10/24.
+//  Copyright © 2019 Microsoft Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DarkModeKit.
+FOUNDATION_EXPORT double DarkModeKitVersionNumber;
+
+//! Project version string for DarkModeKit.
+FOUNDATION_EXPORT const unsigned char DarkModeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarkModeKit/PublicHeader.h>
+
+
---
+++ DarkModeKitTests/DarkModeKitTests.swift
@@ -0,0 +1,12 @@
+//
+//  DarkModeKitTests.swift
+//  DarkModeKitTests
+//
+//  Created by Bei Li on 2019/10/24.
+//  Copyright © 2019 Microsoft Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import DarkModeKit
+
+final class DarkModeKitTests: XCTestCase {}
