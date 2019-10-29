09e9f63727f3a10c8965a31a32641c5a0e2cd275
Bei Li <kylinroc@gmail.com>
2019-10-29 17:52:15 +0800

    Add comments

--- DarkModeKit/Utilities/NSObject+DarkModeKit.h
+++ DarkModeKit/Utilities/NSObject+DarkModeKit.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSObject (DarkModeKit)
 
+/// Swizzle two instance methods for class that calls this method.
+///
+/// Return NO if any selector can not found corresponding method.
 + (BOOL)dm_swizzleInstanceMethod:(SEL)fromSelector to:(SEL)toSelector;
 
 @end
