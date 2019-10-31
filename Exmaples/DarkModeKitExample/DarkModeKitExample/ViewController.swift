5cc276a010878e43cb652c547a3ea12684d2ccf2
Bei Li <kylinroc@gmail.com>
2019-10-31 14:05:31 +0800

    Create example

---
+++ Exmaples/DarkModeKitExample/DarkModeKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeKitExample
+//
+//  Created by Bei Li on 2019/10/31.
+//  Copyright © 2019 Microsoft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
