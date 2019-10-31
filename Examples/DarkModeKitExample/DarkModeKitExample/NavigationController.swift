3abdb737dec8f3d93737bda9866dba0caba8adbe
Bei Li <kylinroc@gmail.com>
2019-10-31 22:21:59 +0800

    Update example’s UI

---
+++ Examples/DarkModeKitExample/DarkModeKitExample/NavigationController.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+
+final class NavigationController: UINavigationController {
+    override var prefersStatusBarHidden: Bool { true }
+}
