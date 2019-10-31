e80488f0bce43ff4aa989edd15b13a85cec8e41a
Bei Li <kylinroc@gmail.com>
2019-10-31 14:08:11 +0800

    Set deployment target to 11.0

--- Exmaples/DarkModeKitExample/DarkModeKitExample/ViewController.swift
+++ Exmaples/DarkModeKitExample/DarkModeKitExample/ViewController.swift
@@ -8,13 +8,4 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+final class ViewController: UIViewController {}
