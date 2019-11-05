1172e08b787d69467aada1e0baa3b59afef293c3
Bei Li <kylinroc@gmail.com>
2019-11-05 15:12:02 +0800

    Add example project (#18)
    * Create example
    
    * Set deployment target to 11.0
    
    * Import DarkModeKit for example
    
    * Update headers
    
    * Correct name
    
    * Update main.yml
    
    * Update main.yml
    
    * Track scheme
    
    * Update example’s UI
    
    * Update example
    
    * Remove storyboard

---
+++ Examples/DarkModeKitExample/DarkModeKitExample/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import DarkModeCore
+import DarkModeKit
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        DarkModeManager.setup(updateAppearance: { _ in })
+        setUpAppearance()
+
+        window = UIWindow()
+        window?.rootViewController = {
+            let tabBarController = UITabBarController()
+            tabBarController.viewControllers = [
+                NavigationController(rootViewController: ViewController()),
+                NavigationController(rootViewController: ViewController()),
+            ]
+            return tabBarController
+        }()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    private func setUpAppearance() {
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = DynamicColor(lightColor: .white, darkColor: .darkGray)
+    }
+}
---
+++ Examples/DarkModeKitExample/DarkModeKitExample/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import DarkModeCore
+import DarkModeKit
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: self,
+            action: #selector(refresh)
+        )
+
+        view.backgroundColor = .white
+    }
+
+    @objc
+    private func refresh() {
+        if DMTraitCollection.current.userInterfaceStyle == .dark {
+            DMTraitCollection.current = DMTraitCollection(userInterfaceStyle: .light)
+        } else {
+            DMTraitCollection.current = DMTraitCollection(userInterfaceStyle: .dark)
+        }
+        DarkModeManager.updateAppearance(for: .shared, animated: true)
+    }
+}
