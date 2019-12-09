bd97dc1b4c59f5d16f61c663fc7af3bc1c1fd9e6
Bei Li <kylinroc@gmail.com>
2019-12-09 18:03:25 +0800

    Add a simple test for color

--- Tests/DarkModeKitTests/DarkModeKitTests.swift
+++ Tests/DarkModeKitTests/DarkModeKitTests.swift
@@ -7,9 +7,39 @@ import XCTest
 @testable import DarkModeKit
 
 final class DarkModeKitTests: XCTestCase {
-  func testSetBackgroundColorSwizzling() {
-    UIWindow.appearance().backgroundColor = .white
-    DarkModeManager.setup()
-    _ = UIWindow()
-  }
+    func testSetBackgroundColorSwizzling() {
+        UIWindow.appearance().backgroundColor = .white
+        DarkModeManager.setup()
+        _ = UIWindow()
+    }
+
+    func testColorInitializer() {
+        let color = UIColor(.dm, light: .white, dark: .black)
+
+        DMTraitCollection.current = DMTraitCollection(userInterfaceStyle: .light)
+        XCTAssertEqual(color.rgba, UIColor.white.rgba)
+
+        DMTraitCollection.current = DMTraitCollection(userInterfaceStyle: .dark)
+        XCTAssertEqual(color.rgba, UIColor.black.rgba)
+    }
+}
+
+extension UIColor {
+    struct RGBA: Equatable {
+        var red: CGFloat
+        var green: CGFloat
+        var blue: CGFloat
+        var alpha: CGFloat
+    }
+
+    var rgba: RGBA {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return RGBA(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
