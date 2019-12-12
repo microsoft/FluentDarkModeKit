9467b9d28edcbd120ddc1528e2fb1ab6febea2a4
Bei Li <kylinroc@gmail.com>
2019-12-12 15:11:23 +0800

    Fix header

--- Tests/DarkModeKitUITests/DarkModeKitUITests.swift
+++ Tests/DarkModeKitUITests/DarkModeKitUITests.swift
@@ -1,9 +1,6 @@
 //
-//  DarkModeKitUITests.swift
-//  DarkModeKitUITests
-//
-//  Created by Bei Li on 2019/12/12.
-//  Copyright © 2019 Microsoft Corporation. All rights reserved.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import XCTest
