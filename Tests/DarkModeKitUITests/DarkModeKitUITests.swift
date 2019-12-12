243d5fe7a1a9e9ce6ad8bad55a06f892aa4391e5
Bei Li <kylinroc@gmail.com>
2019-12-12 15:56:01 +0800

    Add test for UIView

--- Tests/DarkModeKitUITests/DarkModeKitUITests.swift
+++ Tests/DarkModeKitUITests/DarkModeKitUITests.swift
@@ -5,36 +5,94 @@
 
 import XCTest
 
-class DarkModeKitUITests: XCTestCase {
+final class DarkModeKitUITests: XCTestCase {
+  override func setUp() {
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // UI tests must launch the application that they test.
+    // Doing this in setup will make sure it happens for each test method.
+    XCUIApplication().launch()
+  }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+  func testUIView() {
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+    let app = XCUIApplication()
+    let refreshButton = app.navigationBars["DarkModeKitExample.MainView"].buttons["Refresh"]
+    refreshButton.tap()
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    let uiviewStaticText = app.tables.staticTexts["UIView"]
+    uiviewStaticText.tap()
 
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    let screenshot1 = app.screenshot()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    app.navigationBars["DarkModeKitExample.UIViewVC"].buttons["Back"].tap()
+    refreshButton.tap()
+    uiviewStaticText.tap()
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    let tabBarsQuery = app.tabBars
+    tabBarsQuery.children(matching: .button).element(boundBy: 1).tap()
+    app.navigationBars["DarkModeKitExample.View"].buttons["Refresh"].tap()
+    tabBarsQuery.children(matching: .button).element(boundBy: 0).tap()
+
+    let screenshot2 = app.screenshot()
+
+    print(compare(screenshot1.image, screenshot2.image, precision: 1))
+  }
+}
+
+private func compare(_ old: UIImage, _ new: UIImage, precision: Float) -> Bool {
+  guard let oldCgImage = old.cgImage else { return false }
+  guard let newCgImage = new.cgImage else { return false }
+  guard oldCgImage.width != 0 else { return false }
+  guard newCgImage.width != 0 else { return false }
+  guard oldCgImage.width == newCgImage.width else { return false }
+  guard oldCgImage.height != 0 else { return false }
+  guard newCgImage.height != 0 else { return false }
+  guard oldCgImage.height == newCgImage.height else { return false }
+  // Values between images may differ due to padding to multiple of 64 bytes per row,
+  // because of that a freshly taken view snapshot may differ from one stored as PNG.
+  // At this point we're sure that size of both images is the same, so we can go with minimal `bytesPerRow` value
+  // and use it to create contexts.
+  let minBytesPerRow = min(oldCgImage.bytesPerRow, newCgImage.bytesPerRow)
+  let byteCount = minBytesPerRow * oldCgImage.height
+
+  var oldBytes = [UInt8](repeating: 0, count: byteCount)
+  guard let oldContext = context(for: oldCgImage, bytesPerRow: minBytesPerRow, data: &oldBytes) else { return false }
+  guard let oldData = oldContext.data else { return false }
+  if let newContext = context(for: newCgImage, bytesPerRow: minBytesPerRow), let newData = newContext.data {
+    if memcmp(oldData, newData, byteCount) == 0 { return true }
+  }
+  let newer = UIImage(data: new.pngData()!)!
+  guard let newerCgImage = newer.cgImage else { return false }
+  var newerBytes = [UInt8](repeating: 0, count: byteCount)
+  guard let newerContext = context(for: newerCgImage, bytesPerRow: minBytesPerRow, data: &newerBytes) else { return false }
+  guard let newerData = newerContext.data else { return false }
+  if memcmp(oldData, newerData, byteCount) == 0 { return true }
+  if precision >= 1 { return false }
+  var differentPixelCount = 0
+  let threshold = 1 - precision
+  for byte in 0..<byteCount {
+    if oldBytes[byte] != newerBytes[byte] { differentPixelCount += 1 }
+    if Float(differentPixelCount) / Float(byteCount) > threshold { return false}
+  }
+  return true
+}
+
+private func context(for cgImage: CGImage, bytesPerRow: Int, data: UnsafeMutableRawPointer? = nil) -> CGContext? {
+  guard
+    let space = cgImage.colorSpace,
+    let context = CGContext(
+      data: data,
+      width: cgImage.width,
+      height: cgImage.height,
+      bitsPerComponent: cgImage.bitsPerComponent,
+      bytesPerRow: bytesPerRow,
+      space: space,
+      bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+    )
+    else { return nil }
+
+  context.draw(cgImage, in: CGRect(x: 0, y: 0, width: cgImage.width, height: cgImage.height))
+  return context
 }
